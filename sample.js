a7e80ddccc3867c59329c7d4094db5c4e63f9a30
Eliezer Encarnacion <eliezerencarnacion@gmail.com>
2014-09-12 00:31:08 -0400

    Add icons and popup. Plugin correctly retrieves item/user id on sharefile pages

--- sample.js
+++ sample.js
@@ -1,11 +1,39 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.contextMenus.create({
-        "title":"Copy Item Id",
-        "contexts":["link"],
-        "id":"copyId"
-    });
+var errorCheck = function() {
+    if (chrome.runtime.lastError) {
+        console.log(chrome.runtime.lastError);
+    }
+}
+
+//Create context menu option
+chrome.contextMenus.create({
+    "title":"Copy Item/User Id",
+    "contexts":["link"],
+    "id":"copyId",
+    "documentUrlPatterns": [
+        "*://*.sharefiledev.com/*",
+        "*://*.sharefiletest.com/*",
+        "*://*.sharefile.com/*",
+        "*://*.sharefile.local/*",
+    ]
+}, errorCheck);
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if (info.menuItemId == "copyId") {
+        var link = info.linkUrl;
+        var results = link.split("/Index/"); //try to get item id
+        if (results.length == 2) {
+            var itemId = results[1];
+            copyToClipboard(itemId);
+        }
+
+        else if (link.indexOf("id") != -1) {//attempt to get user id
+            var userId = getUserId(link);
+            copyToClipboard(userId);          
+        }
+    }
 });
 
+/* UTIL FUNCTIONS */
 function copyToClipboard(text) {
     var copyDiv = document.createElement('div');
     copyDiv.contentEditable = true;
@@ -18,15 +46,11 @@ function copyToClipboard(text) {
     document.body.removeChild(copyDiv);
 }
 
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    if (info.menuItemId == "copyId") {
-        var link = info.linkUrl;
-        var results = link.split("/Index/");
-        if (results.length == 2) {
-            var itemId = results[1];
-            copyToClipboard(itemId);
-        }
-    }
-});
+function getUserId(url) {
+    console.log(url);
+    var idStartIndex = url.indexOf("id=") + 3;
+    console.log(idStartIndex);
+    var USER_ID_LENGHT = 36;
 
-
+    return url.substring(idStartIndex, idStartIndex + USER_ID_LENGHT);
+}
